8606cb750ee3a97fafc5c0dd2bc5e6287742e3bc
Lars Bergmann <lars.bergmann@perfact.de>
2017-10-19 09:15:47 +0200

    Performance improvements.

--- crawler/crawler.py
+++ crawler/crawler.py
@@ -1,76 +1,108 @@
 import requests
 import threading
 import Queue
-from BeautifulSoup import BeautifulSoup
+#from BeautifulSoup import BeautifulSoup
 import re
+import urlparse
+from lxml import etree
+from StringIO import StringIO
 
 
 class Worker(threading.Thread):
 
-    def __init__(self, inputQue, resultQue, starturl, regex, **kwargs):
+    def __init__(self, inputQue, resultQue, mailQue, host, regex, **kwargs):
         super(Worker, self).__init__(**kwargs)
         self.inputQue = inputQue
         self.resultQue = resultQue
-        self.starturl = starturl
-        self.starturl_stripped = self.starturl.replace('www.', '')
+        self.mailQue = mailQue
+        self.host = host
+        #self.starturl_stripped = self.starturl.replace('www.', '')
         self.regex = regex
+        self.parser = etree.HTMLParser()
+
+        self.bad_endings = [
+            'pdf', 'jpg', 'mp4'
+        ]
+
+    def extract_host(self, url):
+        return urlparse.urlparse(url).netloc
+
+    def extract_links(self, html):
+        '''
+        build tree object from html and yield href atrributes of a tags
+        '''
+        # we need something that behaves like a file here!
+        stringio = StringIO(html)
+        tree = etree.parse(stringio, self.parser)
+        for element in tree.iter():
+            if element.tag != 'a':
+                continue
+            href = element.get('href')
+            if not href:
+                continue
+            if href.split('.')[-1] not in self.bad_endings:
+                yield href
 
     def run(self):
+        '''
+        main method of thread doing its work until inputQue is empty
+        work is:
+        -extract links from a tags
+        -scan html for mail adresses
+        hopefully returns peacefully afterwards
+        '''
         while not self.inputQue.empty():
             url = self.inputQue.get(False)
-            # print 'My url is: %s' % url
+            print 'My url is: %s' % url
             try:
-                result = requests.get(url, allow_redirects=True)
+                result = requests.get(
+                    url,
+                    allow_redirects=True,
+                    timeout=5.0
+                )
                 content = result.content
-                soup = BeautifulSoup(content)
+                #soup = BeautifulSoup(content)
             except Exception, e:
                 print str(e)
                 continue
-            print re.findall(self.regex, content)
 
-            # content = content.decode('utf-8')
-            for link in soup.findAll('a'):
-                link_raw = link.get('href')
-                # print link_raw
-                if link_raw is None:
-                    continue
-                if self.starturl in link_raw or self.starturl_stripped in link_raw:
-                    # print 'new url found: %s' % link_raw
+            # print result.headers.get('Content-Type'), url
+            if 'text/html' not in result.headers.get('Content-Type'):
+                continue
+
+            for mail in re.findall(self.regex, content):
+                self.mailQue.put(mail, False)
+            for link_raw in self.extract_links(content):
+                host = self.extract_host(link_raw)
+                if not host:
+                    # link is relative or something
+                    new_link = urlparse.urljoin(url, link_raw)
+                    #print new_link
+                    if new_link.startswith('http'):
+                        self.resultQue.put(new_link)
+                else:
+                    # link is absoulte
+                    if 'www' not in host:
+                        host = 'www.' + host
+                    if host != self.host:
+                        continue
                     self.resultQue.put(link_raw)
-                    continue
-                if link_raw.startswith('/'):
-                    new_link = self.starturl + link_raw
-                    # print 'relative link found: %s' % new_link
-                    self.resultQue.put(new_link)
-                    continue
-
-                if link_raw.startswith('./'):
-                    new_link = self.starturl + link_raw[1:]
-                    # print 'relative link found: %s' % new_link
-                    self.resultQue.put(new_link)
-                    continue
-
-                if u'/' not in link_raw:
-                    new_link = self.starturl + u'/' + link_raw
-                    # print 'relative link found: %s' % new_link
-                    self.resultQue.put(new_link)
-                    continue
-
-                print link_raw, self.starturl_stripped
-                #new_link = self.starturl + u'/' + link_raw
-                #self.resultQue.put(new_link)
 
 
 def crawl(starturl):
 
     inputQue = Queue.Queue()
     resultQue = Queue.Queue()
+    mailQue = Queue.Queue()
 
     inputQue.put(starturl, False)
 
-    depth = 10
+    depth = 5
     numworkers = 25
     linksdone = []
+    mail_adresses = []
+
+    host = urlparse.urlparse(starturl).netloc
 
     reobj = re.compile(r"\b[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,6}\b", re.IGNORECASE)
     # reobj = re.compile(ur'[^\s@<>]+@[^\s@<>]+\.[^\s@<>]+', re.MULTILINE | re.IGNORECASE)
@@ -79,12 +111,23 @@ def crawl(starturl):
         threadlist = []
 
         for _ in xrange(numworkers):
-            thread = Worker(inputQue, resultQue, starturl, reobj)
+            thread = Worker(
+                inputQue=inputQue,
+                resultQue=resultQue,
+                mailQue=mailQue,
+                host=host,
+                regex=reobj
+            )
+            thread.daemon = True
             thread.start()
             threadlist.append(thread)
 
         for thread in threadlist:
-            thread.join()
+            thread.join(10)
+
+        print '------------------------------'
+        print 'GENERATION FINISHED'
+        print '------------------------------'
 
         while not resultQue.empty():
             new_url = resultQue.get(False)
@@ -92,7 +135,16 @@ def crawl(starturl):
                 inputQue.put(new_url, False)
                 linksdone.append(new_url)
 
+        while not mailQue.empty():
+            new_mail = mailQue.get(False)
+            if new_mail not in mail_adresses:
+                mail_adresses.append(new_mail)
+
     print '########################'
     print 'From starturl %s found following sites:' % starturl
     for link in linksdone:
         print link
+    print '########################'
+    print 'Got mails:'
+    for mail in mail_adresses:
+        print mail
--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 import argparse
 import crawler.crawler as crawler
 import sys
+import time
 
 if __name__ == '__main__':
     if len(sys.argv) > 1:
         url = sys.argv[1]
+        start = time.time()
         crawler.crawl(url.decode('utf-8'))
+        runtime = time.time() - start
+        print('Crawler took %s secdonds' % runtime)
 
     '''
     crawler.crawl('https://www.circle-marketing.de')
